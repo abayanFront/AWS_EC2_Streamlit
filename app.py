5415d106c49220676c1c22c4bdbf2fe9f7d0174e
abayanFront <98825752+abayanFront@users.noreply.github.com>
2022-02-09 22:50:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Hi, ")
-st.write("Front Finance ")
+st.title("Hi, my name is Adar and I am testing docker aws ec2 connection for Front Finance ")
+st.write("Hello Dr. Ozgur Guler")
 
